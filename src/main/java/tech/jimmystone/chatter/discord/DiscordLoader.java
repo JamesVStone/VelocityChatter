fe6c9f21ef526c8050901240ce4da9c085d3a26d
James Stone <jstone@jnet-it.com>
2021-07-16 14:43:22 +0100

    Remove JDA-utilities

--- src/main/java/tech/jimmystone/chatter/discord/DiscordLoader.java
+++ src/main/java/tech/jimmystone/chatter/discord/DiscordLoader.java
@@ -1,8 +1,5 @@
 package tech.jimmystone.chatter.discord;
 
-import com.jagrosh.jdautilities.command.CommandClientBuilder;
-import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
-import com.jagrosh.jdautilities.examples.command.PingCommand;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
@@ -16,17 +13,7 @@ public class DiscordLoader {
 
     public static void startBot(String token) throws LoginException {
         // slash commands don't need intents
-        EventWaiter waiter = new EventWaiter();
-        CommandClientBuilder client = new CommandClientBuilder();
-
-        client.useDefaultGame();
-        client.setOwnerId("519470107379105792");
-        client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
-        client.setPrefix("!");
-        client.addCommands(new PingCommand());
-
         jda = JDABuilder.createDefault(token)
-                .addEventListeners(waiter, client.build())
                 .addEventListeners(new DiscordHandler())
                 .build();
 
